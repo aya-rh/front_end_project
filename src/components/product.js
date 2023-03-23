a6528b67e0b5f670d59705bb95365488d29bfa13
Diana Eboh <dianaeboh@dianas-air.home>
2023-03-23 11:13:11 +0000

    photos

--- src/components/product.js
+++ src/components/product.js
@@ -1,13 +1,21 @@
 const Product = ({product, deleteProduct}) => {
-
+    function importAll(r) {
+        let images = {};
+        r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+        return images;
+      }
+      
+      const pics = importAll(require.context('../pics', false, /\.(png|jpeg|svg)$/));
     return(
         <div className="product-card">
+            <img src={pics[`${product.name}.jpeg`]} alt={product.name}/>
             <h4><strong>Product: </strong>{product.name}</h4>
             <p><strong>Price: £</strong>{product.price}</p>
             <p><strong>Description: </strong>{product.description}</p>
             <p><strong>Seller: </strong>{product.sellerName}</p>
             <button className="add">Add to cart</button>
             <button className="delete" onClick={()=> deleteProduct(product.productId)}>Delete</button>
+           
         </div>
     )
 }
